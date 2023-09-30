ed2504a873833584915062632c7e9bfa203d6e9a
DESKTOP-IADFLHJ\Luis <triskel67@hotmail.com>
2023-09-30 23:28:22 +0200

    sdk 33

--- lGameEngine/src/main/java/com/luis/lgameengine/gameutils/Settings.java
+++ lGameEngine/src/main/java/com/luis/lgameengine/gameutils/Settings.java
@@ -11,6 +11,7 @@ import android.os.Build;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * @author Luis Valdes Frances
@@ -146,7 +147,6 @@ public class Settings {
 
 
         //Hide systemUI
-        /*
         final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -173,7 +173,6 @@ public class Settings {
                         }
                     });
         }
-        */
 
         DisplayMetrics dm = new DisplayMetrics();
         activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
